8a4c91040405dfba7bcad40f38315a54e86c4345
Evanaxander <abir.ehsan11@com>
2026-02-04 11:05:21 +0600

    requirements added

---
+++ research/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Abir Ehsan Evan",
+    author_email="abir.ehsan11@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
